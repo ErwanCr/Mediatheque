14c9e1176f13146471ab04245aca356ca27128f8
Kyllian <47327666+Wyzykiki@users.noreply.github.com>
2019-05-27 16:14:17 +0200

    Ajoue du champs abonnement et update BD.

--- Mediatheque/src/Entity/User/ShopUser.php
+++ Mediatheque/src/Entity/User/ShopUser.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Entity\User;
 
+use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\MappedSuperclass;
 use Doctrine\ORM\Mapping\Table;
 use Sylius\Component\Core\Model\ShopUser as BaseShopUser;
@@ -14,4 +15,17 @@ use Sylius\Component\Core\Model\ShopUser as BaseShopUser;
  */
 class ShopUser extends BaseShopUser
 {
+
+		/** @ORM\Column(type="string", columnDefinition="ENUM('Classique', 'Jeune', 'Premium')", nullable=true) */
+		private $abonnement;
+
+		public function getAbonnement(): ?string
+		{
+			return $this->abonnement;
+		}
+	
+		public function setAbonnement(string $abonnement): void
+		{
+			$this->abonnement = $abonnement;
+		}
 }
